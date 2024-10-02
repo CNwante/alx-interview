6a235ee3603e9bd8c09d7255e22b2cbf02f8691d
Solomon Nwante <myphilothea@gmail.com>
2024-10-02 15:47:30 +0100

    Add documentations

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python3
+"""
+A module for working with Pascal's triangle
+"""
+
+
 def pascal_triangle(n):
     """
     Returns a list of lists of integers representing the Pascal’s triangle
