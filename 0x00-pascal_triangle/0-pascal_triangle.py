f6b84a49b83a7361b0bdce2534bf6d4a997f8fcf
Solomon Nwante <myphilothea@gmail.com>
2024-10-02 13:10:18 +0100

    Submit task 0: generate Pascals triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing the Pascal’s triangle
+    """
+
+    if n <= 0:
+        return []
+    triangle = [[1]]
+
+    for row in range(1, n):
+        prev_row = triangle[-1]
+        current_row = [1]
+
+        for col in range(1, row):
+            current_row.append(prev_row[row - 1] + len(prev_row))
+
+        current_row.append(1)
+        triangle.append(current_row)
+
+    return triangle
