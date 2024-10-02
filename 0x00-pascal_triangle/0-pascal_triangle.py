1f1475eda7940f7995dd7b6c6730676cc611c357
Solomon Nwante <myphilothea@gmail.com>
2024-10-02 16:00:18 +0100

    Fix code error

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -18,7 +18,7 @@ def pascal_triangle(n):
         current_row = [1]
 
         for col in range(1, row):
-            current_row.append(prev_row[row - 1] + len(prev_row))
+            current_row.append(prev_row[col - 1] + prev_row[col])
 
         current_row.append(1)
         triangle.append(current_row)
