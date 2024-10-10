76d6aba224181246d09001122516f8736e5136e8
Solomon Nwante <myphilothea@gmail.com>
2024-10-10 15:00:07 +0100

    Fix pycodestyle

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,18 +5,18 @@ A module for unlocking boxes
 
 
 def canUnlockAll(boxes):
-  """
-  Returns true if all boxes are unlocked, otherwise, false
-  """
-  box_length = len(boxes)
-  opened = set([0])
-  keys = set(boxes[0])
+    """
+    Returns true if all boxes are unlocked, otherwise, false
+    """
+    box_length = len(boxes)
+    opened = set([0])
+    keys = set(boxes[0])
 
-  while keys:
-    new_key = keys.pop()
+    while keys:
+        new_key = keys.pop()
 
-    if new_key not in opened and new_key < box_length:
-      opened.add(new_key)
-      keys.update(boxes[new_key])
+        if new_key not in opened and new_key < box_length:
+            opened.add(new_key)
+            keys.update(boxes[new_key])
 
-  return len(opened) == box_length
+    return len(opened) == box_length
