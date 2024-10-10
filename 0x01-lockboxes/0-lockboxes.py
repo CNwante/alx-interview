57c7c9a7ac42e560dc501b8595fe7d207f17b305
Solomon Nwante <myphilothea@gmail.com>
2024-10-10 14:56:59 +0100

    Submit task 0: Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+A module for unlocking boxes
+"""
+
+
+def canUnlockAll(boxes):
+  """
+  Returns true if all boxes are unlocked, otherwise, false
+  """
+  box_length = len(boxes)
+  opened = set([0])
+  keys = set(boxes[0])
+
+  while keys:
+    new_key = keys.pop()
+
+    if new_key not in opened and new_key < box_length:
+      opened.add(new_key)
+      keys.update(boxes[new_key])
+
+  return len(opened) == box_length
