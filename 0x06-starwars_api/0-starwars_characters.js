f5abeb056d4a8ed41ab0eac980f3784e5a206a25
Solomon Nwante <myphilothea@gmail.com>
2024-11-14 05:58:04 +0100

    Update task solution

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,39 @@
 #!/usr/bin/node
+
 const request = require('request');
-const API_URL = 'https://swapi-api.hbtn.io/api';
 
-if (process.argv.length > 2) {
-  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
-    if (err) {
-      console.log(err);
-    }
-    const charactersURL = JSON.parse(body).characters;
-    const charactersName = charactersURL.map(
-      url => new Promise((resolve, reject) => {
-        request(url, (promiseErr, __, charactersReqBody) => {
-          if (promiseErr) {
-            reject(promiseErr);
-          }
-          resolve(JSON.parse(charactersReqBody).name);
-        });
-      }));
-
-    Promise.all(charactersName)
-      .then(names => console.log(names.join('\n')))
-      .catch(allErr => console.log(allErr));
-  });
+const movieId = process.argv[2];
+if (!movieId) {
+  console.error('Missing movie ID');
+  process.exit(1);
 }
+
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request(url, (err, _, body) => {
+  if (err) return console.error(err);
+
+  const charactersArray = JSON.parse(body).characters;
+
+  // Function to fetch character names sequentially
+  const fetchCharacter = (characterUrl) => {
+    return new Promise((resolve, reject) => {
+      request(characterUrl, (err, _, body) => {
+        if (err) return reject(err);
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
+    });
+  };
+
+  // Sequentially fetch each character's data
+  (async () => {
+    try {
+      for (const character of charactersArray) {
+        await fetchCharacter(character);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  })();
+});
